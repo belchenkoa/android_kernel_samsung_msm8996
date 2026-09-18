390461daf97c6ad572bf7ca140726ad01e65f7ab
belchenkoa <belchenkoa@gmail.com>
2026-09-18 22:47:49 +0300

    fix(gts3llte): force lld for FIPS host utilities

--- scripts/kaslr_fips_hmac.sh
+++ scripts/kaslr_fips_hmac.sh
@@ -70,15 +70,6 @@ hmac_patch() {
 		exit 1
 	fi
 	
-	#file_size=`cat builtime_bytes.bin| wc -c`
-	
-	# Make sure that file size of crypto_hmac.bin is as expected
-	#if [ $total_bytes -ne $file_size ]; then
-		#echo "$0: Bytes mismatch"
-		#exit 1
-	#fi
-	
-	
 	key="The quick brown fox jumps over the lazy dog"
 	
 	# Now, generate the hmac.
@@ -89,9 +80,6 @@ hmac_patch() {
 		exit 1
 	fi
 	
-	# Just, for debugging, print the same hmac on console
-	#openssl dgst -sha256 -hmac "$key" builtime_bytes.bin
-	
 	if [[ ! -f crypto_hmac.bin ]]; then
 		echo "$0 : crypto_hmac.bin does not exist"
 		exit 1
@@ -99,20 +87,13 @@ hmac_patch() {
 	
 	file_size=`cat crypto_hmac.bin| wc -c`
 	
-	# hmac(sha256) produces 32 bytes of hmac 
 	if [ $file_size -ne 32 ]; then
 		echo "$0: Unexpected size of Hash file : " $file_size
 		exit 1
 	fi
 	
-	# Now that we have the hmac, update this hmac into an rodata "builtime_crypto_hmac" varialble
-	# in vmlinux file.
-	# This variable has a place holder 32 bytes that will be over-written with generated hmac.
-	# This way, this build time hmac, will be available as a read-only variable at run-time.
-
 	if  [[ $hmac_offset -le 0 ]]; then echo "$0 : hmac_offset invalid"; exit 1; fi
 	
-	# This does the actual update of hmac into vmlinux file, at given offset
 	$fips_utils -u $vmlinux_var crypto_hmac.bin $hmac_offset
 	retval=$?
 	if [ $retval -ne 0 ]; then
@@ -126,41 +107,18 @@ hmac_patch() {
 }
 
 ################ the kaslr patch oneshot code ####################
-# 1st parameter is the array, 2nd is the output file name
 offsets_sizes_gen(){
 	name=$1[@]
 	array=("${!name}")
 
 	outfile=$2
 	rm -f $outfile
-	
-	#Addresses retrieved must be a valid hex
 	reg='^[0-9A-Fa-f]+$'
 	
-	#Total bytes of all crypto sections scanned. Used later for error checking
-	#total_bytes=0;
-	
-	# For each type of Section : 
-	# first_addr  = Address of first_crypto_text, first_crypto_rodata, etc.
-	# last_addr   = Address of last_crypto_text, last_crypto_rodata etc.
-	# start_addr  = Starting Address of a section within vmlinux
-	# offset      = Offset in vmlinux file where the section begins
-	# file_offset = Offset in vmlinux file where the crypto bytes begins.
-	# size        = size of crypto bytes.
-	
-	# Output is offsets_sizes.txt, of the format
-	#   Section Name   crypto_bytes_offset crypto_bytes_size 
-	#                   (in decimal)        (in decimal)
-	#      .text           2531072            114576
-	#    .rodata           9289648             55388
-	#      :                  :                  :
-	
 	for i in "${array[@]}"; do
-	    
 		var1=var2=var3=var4=var5=""
 		first_addr=last_addr=start_addr=offset=file_offset=size=""
 		k=1
-	        #This loop creates var1, var2 etc and set them to individual strings of a row in array
 		for j in $i; do
 			export var$k=$j 
 			let k+=1	
@@ -179,7 +137,6 @@ offsets_sizes_gen(){
 		if  [[ ! $offset =~ $reg ]]; then echo "$0 : offset invalid"; exit 1; fi
 	
 		if [[ $((16#$first_addr)) -lt $((16#$start_addr)) ]]; then echo "$0 : first_addr < start_addr"; exit 1; fi
-	
 		if [[ $((16#$last_addr)) -le $((16#$first_addr)) ]]; then echo "$0 : last_addr <= first_addr"; exit 1; fi
 	
 		file_offset=`expr $((16#$offset)) + $((16#$first_addr)) - $((16#$start_addr))`
@@ -189,16 +146,12 @@ offsets_sizes_gen(){
 		if  [[ $size -le 0 ]]; then echo "$0 : crypto section size invalid"; exit 1; fi
 	
 		echo "$var1 " $file_offset " " $size >> $outfile
-	
-		#let "total_bytes += `expr $((16#$last_addr)) - $((16#$first_addr))`"
 	done
 	
 	if [[ ! -f $outfile ]]; then
 		echo "$0 : offset_sizes.txt does not exist"
 		exit 1
 	fi
-
-
 }
 
 cal_hmac_offset(){
@@ -251,16 +204,18 @@ if [ $retval -ne 0 ]; then
 	exit 1
 fi
 
-#start patching vmlinux with static patcher, temporary put it here 
-rm -f kaslr_fips
-# Android's prebuilt clang may not have a system "ld" in PATH on the
-# self-hosted builder. Prefer the linker shipped with the same toolchain.
+# These are native host utilities. Android 11's prebuilt clang does not use
+# the system GNU ld; select the LLD shipped with the clang toolchain explicitly.
+# Do not gate this on -x: ld.lld may be a symlink and the previous probe caused
+# clang to fall back to looking for a plain "ld" in PATH.
 HOSTCC_LDFLAGS=""
 HOSTCC_BIN="${HOSTCC%% *}"
-if [[ "$(basename "$HOSTCC_BIN")" == clang* && -x "$(dirname "$HOSTCC_BIN")/ld.lld" ]]; then
+if [[ "$(basename "$HOSTCC_BIN")" == clang* ]]; then
 	HOSTCC_LDFLAGS="-fuse-ld=lld"
 fi
-$HOSTCC $HOSTCC_LDFLAGS -o kaslr_fips  $srctree/scripts/kaslr_fips.c
+
+rm -f kaslr_fips
+$HOSTCC $HOSTCC_LDFLAGS -o kaslr_fips $srctree/scripts/kaslr_fips.c
 retval=$?
 if [ $retval -ne 0 ]; then
 	echo "$0 : $HOSTCC returned error"
@@ -274,7 +229,6 @@ if [ $retval -ne 0 ]; then
 	echo "$0 : $HOSTCC returned error"
 	exit 1
 fi
-
 
 var="__reloc_start"
 var1=`cat $system_map_var|grep -w $var|awk '{print $1}'`
@@ -295,12 +249,9 @@ first_crypto_rodata=`expr $((16#$var4))`
 last_crypto_rodata=`expr $((16#$var5))`
 
 ################# CRYPTO #######################
-# FOR GENERIC CRYPTO FILES                         #awk fields to cut
 fips_crypto[0]=".text first_crypto_text last_crypto_text \$5 \$6"
 fips_crypto[1]=".rodata first_crypto_rodata last_crypto_rodata \$5 \$6"
 fips_crypto[2]=".init.text first_crypto_init last_crypto_init \$4 \$5"
-
-# # FOR ASM CRYPTO FILES
 fips_crypto[3]=".text first_crypto_asm_text last_crypto_asm_text \$5 \$6"
 fips_crypto[4]=".rodata first_crypto_asm_rodata last_crypto_asm_rodata \$5 \$6"
 fips_crypto[5]=".init.text first_crypto_asm_init last_crypto_asm_init \$4 \$5"
@@ -308,14 +259,12 @@ fips_crypto[5]=".init.text first_crypto_asm_init last_crypto_asm_init \$4 \$5"
 offsets_sizes_crypto="offsets_sizes_crypto.txt"
 offsets_sizes_gen fips_crypto $offsets_sizes_crypto
 
-#find hmac_offset in vmlinux for updating
 cal_hmac_offset builtime_crypto_hmac .rodata
 
 rodata_va_addr=`cat vmlinux.elf|grep -w '.rodata'|awk '{print $5}'`
 rodata_file_addr=`cat vmlinux.elf|grep -w '.rodata'|awk '{print $6}'`
 va_to_file=`expr $((16#$rodata_va_addr)) - $((16#$rodata_file_addr))`
 
-# the loop
 for index in {0..63}
 do
 	kaslr_patch $index $va_to_file
@@ -329,5 +278,3 @@ rm -f offsets_sizes*
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 echo ">>>>> Time used for generated all hashes is $(($ELAPSED_TIME)) sec"
-
-# And we are done...
